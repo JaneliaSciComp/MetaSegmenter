0f182771b7e30e4b7074f6fb478dfa3fcc45cdfd
denisovg <gennady.denisov@gmail.com>
2015-02-03 08:37:21 -0500

    Adjusted the procedure for conversion of grayscale image to BW, in order to handle the issue with Matlab's imhist function, which computes incorrectly the edges of bins. Added zero padding at the edges of a BW image, so that the entire image is not a single white component

--- 2DSegmentation/MS_S2D_SegmentNeuralCells.m
+++ 2DSegmentation/MS_S2D_SegmentNeuralCells.m
@@ -13,42 +13,42 @@ catch
     return
 end
 
-frac_black = 0.45;
+frac_black = 0.34;
 if nargin > 1
     frac_black = double(varargin{2});
 end 
 
 % Original image    
 I = imread(image_file); 
-I = I(:,:,1);
-Igr = mat2gray(I);
-disp(['min(I)=' num2str(min(min(I))) ' max(I)=' num2str(max(max(I))) ' mean2(I)=' num2str(mean2(I)) ' std2(I)=' num2str(std2(I))]);
+I = mat2gray(I(:,:,1));
+disp(['min(I)=' num2str(min(min(I))) ' max(I)=' num2str(max(max(I))) ...
+      ' mean2(I)=' num2str(mean2(I)) ' std2(I)=' num2str(std2(I))]);
 In = round(double(I)*255.0/max(max(double(I))));
-disp(['min(In)=' num2str(min(min(In))) ' max(In)=' num2str(max(max(In))) ' mean2(In)=' num2str(mean2(In)) ' std2(In)=' num2str(std2(In))]);
+disp(['min(In)=' num2str(min(min(In))) ' max(In)=' num2str(max(max(In))) ...
+      ' mean2(In)=' num2str(mean2(In)) ' std2(In)=' num2str(std2(In))]);
 imshow(I);
 title('Original image (I)');
 waitforbuttonpress;
 close all;
 
-% Black-white image
-num_edges = 101;
-%V = reshape(I, [1 numel(I)]);
-[N,edges] = my_imhist(I,num_edges);
-%[N,edges] = histcounts(I,num_edges-1)
+% Determine intensity threshold for conversion to black-white image
+% NOTE: imhist assumes that I is a grayscale image, with range of values [0, 1]
+% NOTE2: edges are computed incorrectly by Matlab's imhist function,
+%        so a custom function has been implemented for compuring the edges
 
-Nsum = sum(N);
-disp(['numel(N)=' num2str(numel(N)) ' Nsum=' num2str(Nsum) ' numel(I)=' num2str(numel(I)) ' numel(edges)=' num2str(numel(edges))]);
+num_edges = 1001;
+N     = imhist(I,num_edges);      % counts
+edges = compute_edges(num_edges); % using a custom function for edges
+Nsum  = sum(N);
+disp(['numel(N)=' num2str(numel(N)) ' Nsum=' num2str(Nsum) ...
+      ' numel(I)=' num2str(numel(I)) ' numel(edges)=' num2str(numel(edges))]);
 cum_hist = get_cumulative_histogram(N, Nsum);
-%figure
-%plot(cum_hist);
-%drawnow;
-%waitforbuttonpress;
-%close all;
 
-threshold = get_threshold_intensity(N, frac_black, edges);
+% Black-white image
+threshold = get_threshold_intensity(cum_hist, frac_black, edges);
 disp(['final threshold_intensity=' num2str(threshold)]);
 disp(['size(I)=' num2str(size(I)) ' class(I)=' class(I(1,1))]);
-Ibw = my_im2bw(I, threshold);  
+Ibw = im2bw(I, threshold);  
 disp(['size(Ibw)=' num2str(size(Ibw)) ' class(Ibw)=' class(Ibw(1,1))]);
 Ibw = bwareaopen(Ibw,20); 
 disp(['my frac_black=' num2str(sum(sum(Ibw == 0))/numel(Ibw))]);
@@ -59,13 +59,9 @@ waitforbuttonpress;
 close all;
 
 % Fill holes and open gaps
+Ibw = adjust_edges(Ibw);
 Ibwf = imfill(Ibw, 'holes');
 Ibwf = bwareaopen(Ibwf,20);
-%Ibwfr = imcomplement(Ibwf);
-%Ibwfr = imclose(Ibwfr, strel('disk',1));
-%Ibwf = imcomplement(Ibwfr);
-%Ibwfo = imopen(Ibwf, strel('disk',2));
-%Ibwfo = bwareaopen(Ibwf,20);
 imshow(Ibwf)
 drawnow;
 waitforbuttonpress;
@@ -88,34 +84,30 @@ close all;
 function cum_hist = get_cumulative_histogram(N, Nsum)
     cum_hist = zeros(numel(N));
     my_sum = 0;
-    for i=1:numel(N)
-        my_sum = my_sum + N(i);
+    for i=2:numel(N)
+        my_sum = my_sum + N(i-1);
         cum_hist(i) = my_sum/Nsum; 
     end
+    % Adjusting the last bin
+    my_sum = my_sum + N(i);
+    cum_hist(numel(N)) = my_sum/Nsum;
 
 % -----------------------------------------------------------------------------
 
-function threshold_intensity = get_threshold_intensity(N, frac_black, edges)
-    Ncurr = 0;
-    Nsum = sum(N);
-    threshold_count = double(Nsum) * frac_black;
-    imax = numel(N);
-    disp(['N(1)=' num2str(N(1)) ' N(last)=' num2str(N(imax)) ' N(pre-last)=' num2str(N(imax-1)) ' Nsum=' num2str(Nsum) ' threshold_count=' num2str(threshold_count)]);
+function threshold_intensity = get_threshold_intensity(cum_hist, frac_black, edges)
     threshold_intensity = 0;
-    num_bins = numel(N);
+    num_bins = numel(cum_hist);
     i = 0;
-    while i<= num_bins 
+    while i<= num_bins-1
         i = i + 1;
-        Ncurr = Ncurr + N(i);
-%       disp(['i=' num2str(i) ' frac_black=' num2str(frac_black) ' Ncurr/Nsum=' num2str(Ncurr/Nsum) ' (Ncurr + N(i))/Nsum=' num2str((Ncurr + N(i))/Nsum)]);
-        if Ncurr + N(i) >= threshold_count
-            deltaI = double(edges(i+1) - edges(i));
-            deltaN = double(N(i));
-            threshold_intensity = edges(i) + deltaI*(double(threshold_count - Ncurr)/deltaN);
-            disp(['edges(i)=' num2str(edges(i)) ' deltaI=' num2str(edges(i+1) - edges(i)) ' threshold_intensity=' num2str(threshold_intensity)]); 
+        if cum_hist(i) >= frac_black      
+            deltaI = double(edges(i) - edges(i-1));
+            deltaH = cum_hist(i) - cum_hist(i-1);
+            threshold_intensity = edges(i-1) + deltaI*(frac_black - cum_hist(i-1))/deltaH;           
+            disp(['edges(i)=' num2str(edges(i)) ' cum_hist(i)=' num2str(cum_hist(i))...
+                  ' threshold_intensity=' num2str(threshold_intensity)]);
             break
         end
-        Ncurr = Ncurr + N(i);
     end
 
 % -----------------------------------------------------------------------------
@@ -147,21 +139,51 @@ function L = generate_labels_matrix(Ibwf)
 function Ibw = my_im2bw(I, threshold_intensity)
    Ibw = logical(zeros(size(I)));
    Ibw(I > round(threshold_intensity)) = logical(1);
-   disp(['threshold_intensity=' num2str(threshold_intensity) ' my fraction of black=' num2str((double(numel(Ibw)-sum(sum(Ibw))))/double(numel(Ibw)))]);
+   disp(['threshold_intensity=' num2str(threshold_intensity) ...
+         ' my fraction of black=' num2str((double(numel(Ibw)-sum(sum(Ibw))))/double(numel(Ibw)))]);
 
 % -----------------------------------------------------------------------------
 
-function [N,edges] = my_imhist(I,num_edges);
-    Imax = max(max(I));
-    Imin = min(min(I));
-    step = double(Imax - Imin)/double(num_edges-1);
-    N     = zeros(num_edges-1);
-    edges = zeros(num_edges);
-    for i=1:num_edges
-        edges(i) = double(Imin) + double(i-1)*double(Imax - Imin);
+function edges = compute_edges(num_edges)
+    edges = zeros(1, num_edges);
+    step = 1/double(num_edges-1);
+    edges(1) = 0.;
+    edges(2) = step/2.;
+    edges(num_edges  ) = 1.;
+    edges(num_edges-1) = 1. - step/2.;
+    for i=3:(num_edges-2)
+        edges(i) = edges(i-1) + step;
     end
-    for i=1:(num_edges-1)
-        N(i) = sum(sum(I >= edges(i) & I < edges(i+1)));
+
+% -----------------------------------------------------------------------------
+
+% Add zero padding at the adges, 
+% to ensure thgat that entire image != one BW component
+function Ibw = adjust_edges(Ibw)
+    size1 = size(Ibw, 1);
+    size2 = size(Ibw, 2);
+
+    i = 1;
+    while sum(Ibw(i,:)) >= size2*0.9
+        Ibw(i,:) = logical(0);
+        i = i+1;
     end
-    N(num_edges-1) = N(num_edges-1) + sum(sum(I == edges(num_edges)));
+
+    i = 1;
+    while sum(Ibw(:,i)) >= size1*0.9
+        Ibw(:,i) = logical(0);
+        i = i+1;
+    end
+
+    i = 0;
+    while sum(Ibw(size1 - i,:)) >= size2*0.9
+        Ibw(size1 - i,:) = logical(0);
+        i = i+1;
+    end
+
+    i = 0;
+    while sum(Ibw(:,size2 - i)) >= size1*0.9
+        Ibw(:,size2 - i) = logical(0);
+        i = i+1;
+    end
 
